c5a25fe406ebcb1c38b9d26f218b0293c33ff837
IgneeK <igne.kukankaite@devbridge.com>
2023-02-15 14:22:49 +0200

    iniatial commit

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,5 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        EMAIL1: string;
-        EMAIL2: string;
-        EMAIL3: string;
-        EMAIL4: string;
-        EMAIL5: string;
-        PASSWORD: string;
-        URL: string;
-        BASE_URL: string;
-        CLIENT_ID: string;
-        TENANT_ID: string;
-        CLIENT_SECRET: string;
+        WORD: string;
     }
 }
