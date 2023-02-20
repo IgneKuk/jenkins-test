cdbdb63727d426341ecb56ea254737cf04ccc5e9
IgneeK <igne.kukankaite@devbridge.com>
2023-02-20 08:54:54 +0200

    iniatial commit

---
+++ quizzt-testai/test/jenkins-homepage-fail.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Jenkins Homepage', async ({ page, browserName }) => {
+  await page.goto('https://www.jenkins.io/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/dfsdfdf/);
+
+  // await page.screenshot({ path: 'homepage-'+browserName+'.png', fullPage: true });
+  // console.log(${WORD})
+});
